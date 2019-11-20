eb1b6a35daf3a5dd51c59d5f502a1f4ea96e3cb9
Kevin Scaringi <Kevin@Kevins-MBP.attlocal.net>
2019-11-19 21:51:06 -0800

    modified comments

--- gtts_core.py
+++ gtts_core.py
@@ -1,13 +1,11 @@
-#Import the required module for text
-# to speech conversion
 from gtts import gTTS
-
-# Language in which you want to convert
 language = 'en'
 
 def gtts_function(mytext, filename):
+    # Create text file
     txt_file = open(filename + ".txt",'w')
     txt_file.write(mytext)
     txt_file.close()
+    # Create mp3 file
     mp3_file = gTTS(text=mytext, lang=language, slow=False)
     mp3_file.save(filename + ".mp3")
--- myapp.py
+++ myapp.py
@@ -1,9 +1,5 @@
 from flask import Flask, render_template, request
-
-# import our OCR function
 from ocr_core import ocr_function
-
-# import our gTTs function
 from gtts_core import gtts_function
 
 app = Flask(__name__)
@@ -17,12 +13,10 @@ SERVE_FOLDER = '/static/uploads/'
 # allow files of a specific type
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 
-
 # function to check the file extension
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
 
 # route and function to handle the web page
 @app.route('/', methods=['GET', 'POST'])
@@ -39,18 +33,14 @@ def home():
                                    msg='No file selected')
 
         if file and allowed_file(file.filename):
-
             # save file to static/uploads folder
             file.save(UPLOAD_FOLDER + file.filename)
-
             # call the OCR function for text
             extracted_text = ocr_function(file, UPLOAD_FOLDER + file.filename)
 
             if extracted_text:
-                # call the gtts function for audio
                 gtts_function(extracted_text, UPLOAD_FOLDER + file.filename)
-
-                # return text and audio + updated page
+                # return text, audio, txt file + update page
                 return render_template('index.html',
                                        msg='Successfully processed!',
                                        extracted_text=extracted_text,
@@ -58,7 +48,6 @@ def home():
                                        mp3_file=SERVE_FOLDER + file.filename + ".mp3",
                                        txt_file=SERVE_FOLDER + file.filename + ".txt")
             else:
-               # display error of no text detected
                return render_template('index.html',
                                       msg='No Text Detected',
                                       img_src=SERVE_FOLDER + file.filename)
@@ -67,7 +56,6 @@ def home():
                                    msg='Invalid File Type')
     elif request.method == 'GET':
         return render_template('index.html')
-
 
 if __name__ == '__main__':
     app.run(debug=True)
