714379d14ff4c05d954e8c1e32d42c7f2db188a9
Kevin Scaringi <Kevin@Kevins-MacBook-Pro.local>
2020-01-29 12:39:13 -0800

    big changes!

--- ocr_core.py
+++ ocr_core.py
@@ -1,11 +1,13 @@
 from PIL import Image
 import pytesseract
 
+tessdata_dir_config = '--tessdata-dir "/home/kevinscaringi/tessdata_fast"' #PythonAnywhere
+# tessdata_dir_config = '--tessdata-dir "/Users/Kevin/Documents/tessdata_fast"' #Mac
 def ocr_function(file, filename, language):
     # Convert Image to Grey
     greyImage = Image.open(file).convert('LA')
-    
+
     # Extract text from image
-    text = pytesseract.image_to_string(greyImage, lang=language)
+    text = pytesseract.image_to_string(greyImage, lang=language, config=tessdata_dir_config)
     greyImage.save(filename + "-gray.png")
     return text
