e53de13193d7635e286fa4c34764c36812a399e1
Kevin Scaringi <Kevin@Kevins-MBP.attlocal.net>
2020-06-19 22:58:51 -0700

    ocr-google

--- ocr_google.py
+++ ocr_google.py
@@ -9,6 +9,64 @@ def detect_text(path):
     with io.open(path, 'rb') as image_file:
         content = image_file.read()
 
+    image = vision.types.Image(content=content) #may not need vision
+
+    response = client.text_detection(image=image)
+    texts = response.text_annotations
+    print('Texts:')
+
+    # may not need these blocks
+    for text in texts:
+        print('\n"{}"'.format(text.description))
+
+        vertices = (['({},{})'.format(vertex.x, vertex.y)
+                    for vertex in text.bounding_poly.vertices])
+
+        print('bounds: {}'.format(','.join(vertices)))
+
+
+'''
+#THIS IS A WAY TO DO IT BY URL, WHICH MAY BE EASIER TO SERVE BACK BASED ON THE FILE NAME
+def detect_text_uri(uri):
+    """Detects text in the file located in Google Cloud Storage or on the Web.
+    """
+    from google.cloud import vision
+    client = vision.ImageAnnotatorClient()
+    image = vision.types.Image()
+    image.source.image_uri = uri
+
+    response = client.text_detection(image=image)
+    texts = response.text_annotations
+    print('Texts:')
+
+    for text in texts:
+        print('\n"{}"'.format(text.description))
+
+        vertices = (['({},{})'.format(vertex.x, vertex.y)
+                    for vertex in text.bounding_poly.vertices])
+
+        print('bounds: {}'.format(','.join(vertices)))
+
+    if response.error.message:
+        raise Exception(
+            '{}\nFor more info on error messages, check: '
+            'https://cloud.google.com/apis/design/errors'.format(
+                response.error.message))
+'''
+
+
+'''
+from google.cloud import vision
+import io
+
+def detect_text(path):
+    # Detects text in the file.
+    client = vision.ImageAnnotatorClient()
+
+    # [START vision_python_migration_text_detection]
+    with io.open(path, 'rb') as image_file:
+        content = image_file.read()
+
     image = vision.types.Image(content=content)
 
     response = client.text_detection(image=image)
@@ -22,3 +80,4 @@ def detect_text(path):
                     for vertex in text.bounding_poly.vertices])
 
         print('bounds: {}'.format(','.join(vertices)))
+'''
